09b160e03710ebed98871dfc5153043337bd07ca
student <linda.vanvooren@ngc.com>
2021-05-15 17:08:42 +0000

    Add logic and test

---
+++ src/test/java/com/vanvoo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanvoo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
